cee399a3bd21e1f424b26ef7a4ecaab186a8ecf3
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2011-09-07 18:43:58 +0200

    Project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alumni::Application.config.secret_token = 'f95662a0186569ad40a06df94d3c240fba693d86644ed4e5c49c45250c764c823bed859a1359eb5a50340243b39b9c40678cfd639684c5e882819d9e15f94cd9'
