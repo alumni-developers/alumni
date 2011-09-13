345713d1d460bb0d978aef1f8bbeb9e888bddd14
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2011-09-13 18:19:03 +0200

    static changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Alumni::Application.routes.draw do
+  match '/contact', :to=>'pages#contact'
+  match '/about', :to=> 'pages#about'
+  match '/help', :to=> 'pages#help'
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
