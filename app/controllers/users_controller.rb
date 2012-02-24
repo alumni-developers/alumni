0c71b8b87eb04408ed0ee38a8e8e889f4ea8ad93
Pau Haro Negre <pauharo@gmail.com>
2012-02-23 20:56:20 -0700

    Admin users can also edit any other user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
 
 	def correct_user
 		@user=User.find(params[ :id])
-		redirect_to(root_path) unless current_user?(@user)
+		redirect_to(root_path) unless (current_user?(@user) or current_user.admin?)
 	end
 
   def admin_user
