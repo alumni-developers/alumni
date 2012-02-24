98d0271d4fb62b09e75cf47b30cc3b7d441484bd
Pau Haro Negre <pauharo@gmail.com>
2012-02-23 21:11:12 -0700

    Redirect to user list instead of home.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,10 +59,10 @@ class UsersController < ApplicationController
 
 	def correct_user
 		@user=User.find(params[ :id])
-		redirect_to(root_path) unless (current_user?(@user) or current_user.admin?)
+		redirect_to(users_path) unless (current_user?(@user) or current_user.admin?)
 	end
 
   def admin_user
-    redirect_to(root_path) unless current_user.admin?
+    redirect_to(users_path) unless current_user.admin?
   end
 end
