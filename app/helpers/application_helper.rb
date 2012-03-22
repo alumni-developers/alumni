b97b3d075bf6589a8b9ee80c8ac8842af88ee99a
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2012-03-22 01:31:55 +0100

    CSS: Bootstrap index finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,5 +12,12 @@ module ApplicationHelper
   def logo
     image_tag("logo.jpg",:alt=>"Lo jefe",:class=>"round")
   end
+
+## For bootstrap compatible pagination  
+#  def paginate *params
+#    params[1] = {} if params[1].nil?
+#    params[1][:renderer] = BootstrapPaginationHelper::LinkRenderer
+#    will_paginate *params
+#  end
 end
 
